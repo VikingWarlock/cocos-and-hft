98f1a0b3e1ee4b9e0bd24cff067a39d5995dd6fa
VKWK's New MacBook <vikingwarlock@gmail.com>
2018-06-15 16:34:43 +0800

    controller and game server is linked

--- src/controller.js
+++ src/controller.js
@@ -1,19 +1,31 @@
-const GameClient = hft.GameClient;
-const g_client = new GameClient();
-var inputElem = document.getElementById("inputarea");
+"use strict";
 
+requirejs(['./node_modules/happyfuntimes/dist/hft.js', './node_modules/hft-sample-ui/dist/sample-ui.js']
+    , function (hft, sampleUI) {
+        const GameClient = hft.GameClient;
+        var Input = sampleUI.input;
+
+        const g_client = new GameClient();
+        var inputElem = document.getElementById("inputarea");
+
+        g_client.on("connect",function(){
+           console.log("connected");
+        });
 
 // Send a message to the game when the screen is touched
-inputElem.addEventListener('pointerdown', function (event) {
-    var position = Input.getRelativeCoordinates(event.target, event);
-    sendMoveCmd(position, event.target);
-    event.preventDefault();
-});
+        inputElem.addEventListener('pointerdown', function (event) {
+            var position = Input.getRelativeCoordinates(event.target, event);
+            sendMoveCmd(position, event.target);
+            event.preventDefault();
+        });
 
 
-var sendMoveCmd = function (position, target) {
-    client.sendCmd('move', {
-        x: position.x / target.clientWidth,
-        y: position.y / target.clientHeight,
+        var sendMoveCmd = function (position, target) {
+            g_client.sendCmd('move', {
+                x: position.x / target.clientWidth,
+                y: position.y / target.clientHeight,
+            });
+        };
+
     });
-};
+
